5188a398261543963b454b06c07d7e57da111a44
haydenlinder <haydenlinder@gmail.com>
2020-03-14 14:07:15 -0700

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("You are connected"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));

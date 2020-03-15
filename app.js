caf4b77b02138ec9a598b2f39f9c3ce5a3144dc4
haydenlinder <haydenlinder@gmail.com>
2020-03-14 20:45:29 -0700

    add user auth and frontend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users")
@@ -11,9 +12,10 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize())
+require('./config/passport')(passport);
 
-app.get("/", (req, res) => res.send("You are connected"));
-app.use("/api/users", users)
+app.use("/api/users", users)    
 app.use("/api/tickets", tickets)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

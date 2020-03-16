d1bf9609bb037ce6121fff3b6592504a327d2f1c
haydenlinder <haydenlinder@gmail.com>
2020-03-16 15:00:58 -0700

    create tag actions

---
+++ frontend/src/actions/tag_actions.js
@@ -0,0 +1,37 @@
+import * as tagAPIUtil from '../util/tag_api_util';
+
+export const RECEIVE_TAG = 'RECEIVE_TAG';
+export const RECEIVE_TAGS = 'RECEIVE_TAGS';
+export const RECEIVE_TAG_ERRORS = 'RECEIVE_TAG_ERRORS';
+
+const receiveTag = tag => ({
+    type: RECEIVE_TAG,
+    tag
+})
+
+const receiveTags = tags => ({
+    type: RECEIVE_TAGS,
+    tag
+})
+
+const receiveTagErrors = errors => ({
+    type: RECEIVE_TAG_ERRORS,
+    errors
+})
+
+export const createTag = tag => dispatch => (
+    tagAPIUtil.createTag(tag)
+    .then(tag => dispatch(receiveTag(tag)))
+    .catch(errors = dispatch(receiveTagErrors(errors)))
+)
+
+export const getTags = () => dispatch => (
+    tagAPIUtil.getTags()
+    .then(tags => dispatch(receiveTags(tags)))
+)
+
+export const getTag = (name) => dispatch => (
+    tagAPIUtil.getTag(name)
+    .then(tag => dispatch(receiveTag(tag)))
+    .catch(errors = dispatch(receiveTagErrors(errors)))
+)
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer'
-import ticktsReducer from './tickets_reducer'
+import ticketsReducer from './tickets_reducer'
 
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    tickets: ticktsReducer
+    tickets: ticketsReducer
 })
 
 export default entitiesReducer;

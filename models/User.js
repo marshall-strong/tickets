58cf65bf4402dc0de4ca24794eb70b128a9cec70
jjohnston <jpjohnston@ucdavis.edu>
2020-03-16 22:06:39 -0700

    Update User

--- models/User.js
+++ models/User.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true

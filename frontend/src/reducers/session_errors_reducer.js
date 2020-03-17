1c124677ee74efb45a988bdfebaf42998fce0078
jjohnston <jpjohnston@ucdavis.edu>
2020-03-16 19:22:22 -0700

    Edit session reducer

--- frontend/src/reducers/session_errors_reducer.js
+++ frontend/src/reducers/session_errors_reducer.js
@@ -1,20 +1,33 @@
 import {
-    RECEIVE_SESSION_ERRORS,
     RECEIVE_CURRENT_USER,
+    RECEIVE_USER_LOGOUT,
+    RECEIVE_USER_SIGN_IN
 } from '../actions/session_actions';
 
-const _nullErrors = [];
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+};
 
-const SessionErrorsReducer = (state = _nullErrors, action) => {
-    Object.freeze(state);
+export default function (state = initialState, action) {
     switch (action.type) {
-        case RECEIVE_SESSION_ERRORS:
-            return action.errors;
         case RECEIVE_CURRENT_USER:
-            return _nullErrors;
+            return {
+                ...state,
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            };
+        case RECEIVE_USER_LOGOUT:
+            return {
+                isAuthenticated: false,
+                user: undefined
+            };
+        case RECEIVE_USER_SIGN_IN:
+            return {
+                ...state,
+                isSignedIn: true
+            }
         default:
             return state;
     }
-};
-
-export default SessionErrorsReducer
+}

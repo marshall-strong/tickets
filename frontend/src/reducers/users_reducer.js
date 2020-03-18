d08e000680efd0d0e8bbf8fe8dffb9553ad6c378
haydenlinder <haydenlinder@gmail.com>
2020-03-18 14:15:04 -0700

    add current user to state

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,11 +1,11 @@
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_CURRENT_USER, RECEIVE_USER_LOGOUT } from "../actions/session_actions";
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     let newState = Object.assign({}, state) 
-    switch (action) {
+    switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            newState[action.currentUser.id] = action.currentUser
+            newState[action.payload.id] = action.payload
             return newState;
         default:
             return state;

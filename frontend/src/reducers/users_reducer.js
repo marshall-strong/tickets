382f5fad4714b6b2e4a0c4062afbcff8c143ea23
haydenlinder <haydenlinder@gmail.com>
2020-03-18 16:53:27 -0700

    remove debugger

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -3,7 +3,6 @@ import { RECEIVE_CURRENT_USER, RECEIVE_USER_LOGOUT } from "../actions/session_ac
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     let newState = Object.assign({}, state) 
-    debugger
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             newState[action.payload.id] = action.payload

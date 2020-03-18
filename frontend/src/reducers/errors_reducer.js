f68f99915809d5119ac46ac0dfb639e6a73ceb32
jjohnston <jpjohnston@ucdavis.edu>
2020-03-18 10:03:10 -0700

    Comment frontend

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import tagErrorsReducer from './tag_errors_reducer';
 import SessionErrorsReducer from './session_errors_reducer'
 import ticketErrorsReducer from "./ticket_errors_reducer";
+import commentErrorsReducer from "./comment_errors_reducer"
 
 const errorsReducer = combineReducers({
     tags: tagErrorsReducer,
     session: SessionErrorsReducer,
-    tickets: ticketErrorsReducer
+    tickets: ticketErrorsReducer,
+    comments: commentErrorsReducer
 })
 
 export default errorsReducer;

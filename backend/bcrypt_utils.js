d95d235d1e08f8ed6f1038f6d34b012de000ee41
marstrong <marshallstrong123@gmail.com>
2020-03-19 16:28:59 -0700

    merge master

--- backend/bcrypt_utils.js
+++ backend/bcrypt_utils.js
@@ -1,28 +1,4 @@
-const bcrypt = require('bcrypt');
-
-// const saltRounds = 10;
-// const myPlaintextPassword = 's0/\/\P4$$w0rD';
-// const someOtherPlaintextPassword = 'not_bacon';
-
-// bcrypt.genSalt(saltRounds, function (err, salt) {
-//     bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
-//         // Store hash in your password DB.
-//     });
-// });
-
-
-// bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-//     // Store hash in your password DB.
-// });
-
-
-// // Load hash from your password DB.
-// bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
-//     // result == true
-// });
-// bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
-//     // result == false
-// });
+const bcrypt = require('bcryptjs');
 
 const genHashedPassword = (plaintextPassword) => {
     const saltRounds = 10;
@@ -31,4 +7,4 @@ const genHashedPassword = (plaintextPassword) => {
     return hash;
 };
 
-export { genHashedPassword };
+module.exports = genHashedPassword;

466c5cb485ead428d57f8736803d681318dabef8
your name here <bradnelson333@gmail.com>
2020-03-19 23:18:01 -0700

    index item

--- frontend/src/components/comments/comment_index_item.jsx
+++ frontend/src/components/comments/comment_index_item.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CommentIndexItem = props => {
     return (
         <div>
-            {props.ticket.comments}
+            {props.comment.body}
         </div>
     )
 } 

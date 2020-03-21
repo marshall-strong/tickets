b80989ed1e0c6d6865862ce565f8d83a70d2ee60
marstrong <marshallstrong123@gmail.com>
2020-03-21 15:15:07 -0700

    cleaned up app.js entry file

--- app.js
+++ app.js
@@ -1,51 +1,12 @@
-const seeder = require('mongoose-seed');
-const mongoDbUri = require('./config/keys').development.mongoURI;
-
-const organizationSeeds = require('./backend/mongoose-seed/organizations');
-const userSeeds = require('./backend/mongoose-seed/users');
-
-// Data array containing seed data - documents organized by Model
-const data = [
-    organizationSeeds,
-    userSeeds,
-];
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const passport = require('passport')
-
-const db = require('./config/keys').stage.mongoURI;
+const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users")
 const tickets = require("./routes/api/tickets")
 const tags = require('./routes/api/tags')
 const comments = require('./routes/api/comments')
-
-// // Connect to MogoDB via Mongoose
-// seeder.connect(mongoDbUri, function () {
-
-//     // Load Mongoose models
-//     seeder.loadModels([
-//         './models/organization.js',
-//         './models/user.js',
-//     ]);
-
-//     // Clear specified collections
-//     seeder.clearModels(['Organization', 'User'], function () {
-
-//         // Callback function to populate DB once collections have been cleared
-//         seeder.populateModels(data, () => {
-//             seeder.disconnect();
-//             console.log("seeder disconnected");
-//         });
-
-//     });
-
-//     // seeder.disconnect(() => console.log("seeder disconnected 2"));
-// });
-
-
-
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -61,7 +22,6 @@ app.use('/api/tags', tags)
 app.use('/api/comments', comments)
 
 app.listen(port, () => console.log(`app.listen: Server is running on port ${port}`));
-
 
 mongoose
     .connect(db, { useNewUrlParser: true })

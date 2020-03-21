9fed0ee87939cb29223a1077b75521035bde1745
marstrong <marshallstrong123@gmail.com>
2020-03-21 12:16:25 -0700

    need to merge master updates

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+const seeder = require('mongoose-seed');
+const mongoDbUri = require('./config/keys').test.mongoURI;
+
+// Data array containing seed data - documents organized by Model
+const data = [
+    {
+        model: 'Organization',
+        documents: [
+            {
+                handle: "cats4humanity.org",
+                name: "Cats for Humanity",
+                motto: "a cat in every household!"
+            },
+            {
+                handle: "acme.boom",
+                name: "ACME",
+                motto: "next time..."
+            },
+            {
+                handle: "gmail.com",
+                name: "Google",
+                motto: "if you have a gmail account, you work at Google!"
+            },
+            {
+                handle: "appacademy.io",
+                name: "appAcademy",
+                motto: "appAcademy: if you liked your old life, you wouldn't be here."
+            },
+        ]
+    }
+];
+
+// Connect to MogoDB via Mongoose
+const runMongooseSeed = seeder.connect(mongoDbUri, function () {
+
+    // Load Mongoose models
+    seeder.loadModels([
+        '../models/organization.js',
+    ]);
+
+    // Clear specified collections
+    seeder.clearModels(['Organization',], function () {
+
+        // Callback function to populate DB once collections have been cleared
+        seeder.populateModels(data, function () {
+            seeder.disconnect();
+        });
+
+    });
+});
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
@@ -23,6 +76,7 @@ app.use('/api/tags', tags)
 app.use('/api/comments', comments)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 
 mongoose
     .connect(db, { useNewUrlParser: true })

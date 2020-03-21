9bf6f43cfadbf1ef7e4c558a6b531095232b9003
marstrong <marshallstrong123@gmail.com>
2020-03-21 16:29:43 -0700

    edited seeder.js to populate all models

--- backend/mongoose-seed/seeder.js
+++ backend/mongoose-seed/seeder.js
@@ -1,12 +1,19 @@
-const seeder = require('mongoose-seed');
 const mongoDbUri = require('../../config/keys').development.mongoURI;
+const seeder = require('mongoose-seed');
+
 const organizationSeeds = require('./organizations');
 const userSeeds = require('./users');
+const ticketSeeds = require('./tickets');
+const commentSeeds = require('./comments');
+const tagSeeds = require('./tags');
 
 // Data array containing seed data - documents organized by Model
 const data = [
     organizationSeeds,
     userSeeds,
+    ticketSeeds,
+    commentSeeds,
+    tagSeeds
 ];
 
 // Connect to MogoDB via Mongoose
@@ -16,10 +23,19 @@ seeder.connect(mongoDbUri, function () {
     seeder.loadModels([
         './models/organization.js',
         './models/user.js',
+        './models/ticket.js',
+        './models/comment.js',
+        './models/tag.js'
     ]);
 
     // Clear specified collections
-    seeder.clearModels(['Organization', 'User'], function () {
+    seeder.clearModels([
+        'Organization', 
+        'User',
+        'Ticket',
+        'Comment',
+        'Tag',
+    ], function () {
 
         // Callback function to populate DB once collections have been cleared
         seeder.populateModels(data, () => {

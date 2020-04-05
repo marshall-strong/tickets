2fcb9d21e8bb0517e37743c5a59d8fac2899cbf0
marstrong <marshallstrong123@gmail.com>
2020-04-05 13:08:02 -0700

    reducer formatting and standardization

--- frontend/src/reducers/tags_reducer.js
+++ frontend/src/reducers/tags_reducer.js
@@ -1,21 +1,23 @@
-import { RECEIVE_TAGS, RECEIVE_TAG } from "../actions/tag_actions";
+import { RECEIVE_TAGS, RECEIVE_TAG } from '../actions/tag_actions';
 
 
 const tagsReducer = (state = {}, action) => {
-    Object.freeze(state);
-    let nextState = Object.assign({}, state);
-    switch (action.type) {
-        case RECEIVE_TAGS:
-            for (let i = 0; i < action.tags.length; i++) {
-                nextState[action.tags[i].name] = action.tags[i]
-            }
-            return nextState; 
-        case RECEIVE_TAG:
-            nextState[action.tag.name] = action.tag;
-            return nextState;      
-        default:
-            return state;
-    }
-}
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RECEIVE_TAGS:
+      for (let i = 0; i < action.tags.length; i++) {
+        nextState[action.tags[i].name] = action.tags[i];
+      }
+      return nextState; 
+    case RECEIVE_TAG:
+      nextState[action.tag.name] = action.tag;
+      return nextState;      
+    default:
+      return state;
+  }
+};
+
 
 export default tagsReducer;

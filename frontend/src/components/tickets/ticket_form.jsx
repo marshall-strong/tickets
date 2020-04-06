8d012ac9630125fa28b4a8acfca94cfa4b1e0ca2
haydenlinder <haydenlinder@gmail.com>
2020-04-06 08:14:20 -0700

    Remove code

--- frontend/src/components/tickets/ticket_form.jsx
+++ frontend/src/components/tickets/ticket_form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TicketActivityContainer from "./ticket_activity_container"
 import CommentFormContainer from "../comments/comment_form_container"
-import LastUpdateSeenByContainer from './last_update_seen_by_container'
+import LastUpdateSeenBy from './last_update_seen_by'
 import {withRouter} from "react-router-dom"
 
 import '../app.css'
@@ -347,7 +347,7 @@ class TicketForm extends React.Component {
                 <ul className="activity-container">
                     <div className="activity-header">
                         <h1 className="title">Comments and activity</h1>
-                        <LastUpdateSeenByContainer />
+                        <LastUpdateSeenBy ticket={this.props.ticket}/>
                     </div>
                     <CommentFormContainer />
                     <TicketActivityContainer currentUser={this.props.currentUser}/>

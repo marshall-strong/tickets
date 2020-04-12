20259cc90a9a190532204d1c8ab0bb6cb47d88d6
marstrong <marshallstrong123@gmail.com>
2020-04-11 21:25:58 -0700

    PublicRoute added, AuthRoute ProtectedRoute refactored

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { PublicRoute, AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Route, Switch } from 'react-router-dom';
 
 import NavBarContainer from './navbar/navbar_container';

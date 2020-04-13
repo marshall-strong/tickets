136d02234642f87db2d66c72c62bf8777059370a
marstrong <marshallstrong123@gmail.com>
2020-04-13 00:00:33 -0700

    refactor seeder

--- backend/mongoose-seed/seeder.js
+++ backend/mongoose-seed/seeder.js
@@ -1,7 +1,9 @@
-const mongoDbUri = require('../../config/keys').mongoURI;
 const mongoose = require('mongoose');
+
 const seeder = require('./index');
 // const seeder = require('mongoose-seed');
+
+const KEYS = require('../../config/keys');
 
 const organizationSchema = require('../schemas/organization');
 const userSchema = require('../schemas/user');
@@ -15,15 +17,53 @@ const ticketSeeds = require('./tickets');
 const commentSeeds = require('./comments');
 const tagSeeds = require('./tags');
 
-// Data array containing seed data - documents organized by Model
-const data = [
-    organizationSeeds,
-    userSeeds,
-    ticketSeeds,
-    commentSeeds,
-    tagSeeds
+const Organization = mongoose.model('Organization', organizationSchema);
+const User = mongoose.model('User', userSchema);
+const Ticket = mongoose.model('Ticket', ticketSchema);
+const Comment = mongoose.model('Comment', commentSchema);
+const Tag = mongoose.model('Tag', tagSchema);
+
+
+// collections to clear before seeding
+const dbCollections = [
+  'Organization',
+  'User',
+  'Ticket',
+  'Comment',
+  'Tag',
 ];
 
+// Data array containing seed data - documents organized by Model
+const dbSeedData = [
+  organizationSeeds,
+  userSeeds,
+  ticketSeeds,
+  commentSeeds,
+  tagSeeds
+];
+
+
+// Callback function to populate DB once collections have been cleared
+const dbPopulate = () => {
+  seeder.populateModels(dbSeedData, () => {
+    seeder.disconnect();
+    console.log("seeder disconnected...");
+  });
+};
+
+
+const dbSeed = () => {
+  // seeder.loadModels is no longer needed -- models are loaded above.
+  seeder.loadModels( [] );
+  console.log("Schemas registered for all mongoose models")
+
+  // Clear existing collections, then populate db using seed data
+  seeder.clearModels(dbCollections, dbPopulate);
+};
+
+
+// Connect to MogoDB via Mongoose
+const dbConnectionURI = KEYS.mongoURI;
 const dbConnectionOptions = {
   'useNewUrlParser': true,
   'useFindAndModify': false,
@@ -31,43 +71,4 @@ const dbConnectionOptions = {
   'useUnifiedTopology': true,
 };
 
-const Organization = mongoose.model('Organization', organizationSchema);
-const User = mongoose.model('User', userSchema);
-const Ticket = mongoose.model('Ticket', ticketSchema);
-const Comment = mongoose.model('Comment', commentSchema);
-const Tag = mongoose.model('Tag', tagSchema);
-
-const run = () => {
-  // Load Mongoose models
-
-
-  seeder.loadModels([]
-    // [
-    //   './backend/models/organization.js',
-    //   './backend/models/user.js',
-    //   './backend/models/ticket.js',
-    //   './backend/models/comment.js',
-    //   './backend/models/tag.js'
-    // ]
-  );
-
-  // Clear specified collections
-  seeder.clearModels([
-    'Organization',
-    'User',
-    'Ticket',
-    'Comment',
-    'Tag',
-  ], function () {
-
-    // Callback function to populate DB once collections have been cleared
-    seeder.populateModels(data, () => {
-      seeder.disconnect();
-      console.log("seeder disconnected");
-    });
-
-  });
-};
-
-// Connect to MogoDB via Mongoose
-seeder.connect( mongoDbUri, run);
+seeder.connect(dbConnectionURI, dbConnectionOptions, dbSeed);

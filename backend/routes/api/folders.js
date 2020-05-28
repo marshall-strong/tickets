1168edd48466589a45e2d548b959879c0b1f7b55
haydenlinder <haydenlinder@gmail.com>
2020-05-28 08:36:15 -0700

    Add folders backend

--- backend/routes/api/folders.js
+++ backend/routes/api/folders.js
@@ -5,6 +5,31 @@ const Folder = require("../../models/folder");
 
 router.post('/', (req, res) => {
     const { errors, isValid } = validateFolderInput(req.name);
+    if (!isValid) {
+        return res.status(422).json(errors)
+    }
 
+    const newFolder = new Folder ({
+        name: req.name,
+        creator: req.creator,
+        subscribed: [req.creator]
+    })
 
+    newFolder.save()
+    .then(folder => {
+        res.json(folder)
+    })
 });
+
+router.get('/:userId', (req, res) => {
+    Folder.find({ creator: req.params.userId })
+    .then(folders => res.json(folders))
+})
+
+router.delete('/folders/:folderId', (req, res) => {
+    Folder.findById(req.params.id)
+    .then(folder => {
+        folder.remove()
+        return res.json('success')
+    })
+})

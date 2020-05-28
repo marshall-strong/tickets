bd87d871688d54effa15fd5e495e559ab30b0951
haydenlinder <haydenlinder@gmail.com>
2020-05-28 09:16:58 -0700

    Add folders reducer

---
+++ frontend/src/reducers/folders_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_FOLDER, RECEIVE_FOLDERS, REMOVE_FOLDER } 
+from '../actions/folder_actions'
+
+const foldersReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state);
+
+    switch(action.type){
+        case(RECEIVE_FOLDER):
+            return action.folder;
+        case(RECEIVE_FOLDERS):
+            return action.payload;
+        case(REMOVE_FOLDER):
+            delete nextState[action.id];
+            return nextState;
+        default: 
+            return state;
+    }
+}
+
+export default foldersReducer;
